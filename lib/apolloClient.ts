6e1f1cadf5fc72f28d3a95a5aa5b7b646bead7b5
KOHEI183 <zbtbfuufp@gmail.com>
2023-01-22 16:45:51 +0900

    getQueryを記述

--- lib/apolloClient.ts
+++ lib/apolloClient.ts
@@ -1,3 +1,6 @@
+/**
+ * ハスラ(GQLサーバー)とnextjsを連携するファイル
+ */
 import {
   ApolloClient,
   HttpLink,
@@ -9,9 +12,10 @@ import 'cross-fetch/polyfill'
 let apolloClient: ApolloClient<NormalizedCacheObject> | undefined
 const createApolloClient = () => {
   return new ApolloClient({
+    // windowはブラウザで実行している場合　falseの場合サーバー側での処理をしていることになる
     ssrMode: typeof window === 'undefined',
     link: new HttpLink({
-      uri: process.env.NEXT_PUBLIC_HASURA_URL,
+      uri: process.env.NEXT_PUBLIC_HASURA_URL, //hasuraのendpoint
       headers: {
         'x-hasura-admin-secret': process.env.NEXT_PUBLIC_HASURA_KEY,
       },
@@ -19,9 +23,13 @@ const createApolloClient = () => {
     cache: new InMemoryCache(),
   })
 }
+
+// ssr,ssg,isrの場合は毎回ApolloClientをインスタンス化しないといけない
+// くアイアンとサイドの場合は一回のみ
 export const initializeApollo = (initialState = null) => {
+  // ?? nullまたはundefinedは右辺なにか入っている場合は左辺が入る
   const _apolloClient = apolloClient ?? createApolloClient()
-  // For SSG and SSR always create a new Apollo Client
+  // サーバーサイドの処理の場合
   if (typeof window === 'undefined') return _apolloClient
   // Create the Apollo Client once in the client
   if (!apolloClient) apolloClient = _apolloClient
--- pages/hasura-main.tsx
+++ pages/hasura-main.tsx
@@ -2,26 +2,48 @@ import { VFC } from 'react'
 import Link from 'next/link'
 import { useQuery } from '@apollo/client'
 import { GET_USERS } from '../queries/queries'
+// 自動生成された型
 import { GetUsersQuery } from '../types/generated/graphql'
 import { Layout } from '../components/Layout'
 
 const FetchMain: VFC = () => {
+  // useQueryを叩いた段階でcacheに保存される
+  // __typenameとidでキャシュに存在しているか見に行くメカニズムになっている
   const { data, error } = useQuery<GetUsersQuery>(GET_USERS, {
     //fetchPolicy: 'network-only',
     fetchPolicy: 'cache-and-network',
     //fetchPolicy: 'cache-first',
     //fetchPolicy: 'no-cache',
   })
+
+  // const { data, error } = useQuery<GetUsersQuery>(
+  //   gql`
+  //     query GetUsers {
+  //       users(order_by: { created_at: desc }) {
+  //         id
+  //         name
+  //         created_at
+  //       }
+  //     }
+  //   `,
+  //   {
+  //     //fetchPolicy: 'network-only',
+  //     fetchPolicy: 'cache-and-network',
+  //     //fetchPolicy: 'cache-first',
+  //     //fetchPolicy: 'no-cache',
+  //   }
+  // )
+
   if (error)
     return (
       <Layout title="Hasura fetchPolicy">
         <p>Error: {error.message}</p>
       </Layout>
     )
+  console.warn(data, 'data')
   return (
     <Layout title="Hasura fetchPolicy">
       <p className="mb-6 font-bold">Hasura main page</p>
-
       {data?.users.map((user) => {
         return (
           <p className="my-1" key={user.id}>
